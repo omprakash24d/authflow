c6af5d4522ba10e84a9d2900bc12a5bf109d581d
Om Prakash <dr.omprakashmbbs@gmail.com>
2025-06-20 17:06:41 +0000

    Some of the files's code are very large can you break down those in to s

---
+++ src/components/ui/sidebar/index.ts
@@ -0,0 +1,17 @@
+
+export * from "./context";
+export * from "./main";
+export * from "./trigger";
+export * from "./rail";
+export * from "./inset";
+export * from "./input";
+export * from "./header";
+export * from "./footer";
+export * from "./separator";
+export * from "./content";
+export * from "./group";
+export * from "./menu";
+export * from "./menu-button";
+export * from "./menu-action";
+export * from "./menu-badge";
+export * from "./menu-sub";
