8cbd882e01068c9998f4067625429c3cf45850f2
Om Prakash <dr.omprakashmbbs@gmail.com>
2025-06-21 05:27:26 +0000

    Can you use this image as favicon for my project: public/images/AuthFlow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ import type { PropsWithChildren } from 'react';
 export const metadata: Metadata = {
   title: 'AuthFlow', // Default title for the application
   description: 'Comprehensive User and Authentication System by Firebase Studio', // Default description
+  icons: {
+    icon: '/images/AuthFlow.svg',
+  },
 };
 
 /**
